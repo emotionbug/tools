635ef5f876462304ee0d71cdc9283867cd5053a1
Alex Kwak <take-me-home@kakao.com>
2024-05-14 10:52:54 +0900

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -4,4 +4,5 @@ tar -zxvf tmpbackup.tar.gz
 gpg --import tmpbackup/secret.gpg
 cp -Rf tmpbackup/.ssh/* ~/.ssh
 cp -f tmpbackup/.gitconfig ~/.gitconfig
+cp -Rf tmpbackup/devdotenv ~/devdotenv
 rm -rf tmpbackup
