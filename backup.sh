7d5419023fd1d96c523cb037d3ccb7cb0ab216db
Alex Kwak <take-me-home@kakao.com>
2024-05-14 10:52:11 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 mkdir tmpbackup
+# GPG Private key
 gpg --export-options backup -o tmpbackup/secret.gpg --export-secret-keys
+# SSH_Key
 cp -R ~/.ssh tmpbackup/
+# GitConfig
 cp .gitconfig tmpbackup/
+# DevEnv
+mkdir -p devdotenv
+cp -R devdotenv tmpbackup/
+
 tar -zcvf tmpbackup.tar.gz tmpbackup
