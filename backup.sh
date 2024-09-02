f07a17442bca6a880f84e3ae47778955f9dda2a2
Alex Kwak <take-me-home@kakao.com>
2024-09-02 15:00:35 +0900

    add sops

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf tmpbackup 
+rm -rf tmpbackup
 rm -rf tmpbackup.tar.gz
 mkdir -p tmpbackup
 # GPG Private key
@@ -8,9 +8,11 @@ gpg --export-options backup -o tmpbackup/secret.gpg --export-secret-keys
 # SSH_Key
 cp -R ~/.ssh tmpbackup/
 # GitConfig
-cp .gitconfig tmpbackup/
-# DevEnv
-mkdir -p devdotenv
-cp -R devdotenv tmpbackup/
+cp ~/.gitconfig tmpbackup/
+
+# Configs
+mkdir -p ~/.config/sops
+mkdir -p tmpbackup/.config
+cp -R ~/.config/sops tmpbackup/.config
 
 tar -zcvf tmpbackup.tar.gz tmpbackup
--- restore.sh
+++ restore.sh
@@ -4,5 +4,5 @@ tar -zxvf tmpbackup.tar.gz
 gpg --import tmpbackup/secret.gpg
 cp -Rf tmpbackup/.ssh/* ~/.ssh
 cp -f tmpbackup/.gitconfig ~/.gitconfig
-cp -Rf tmpbackup/devdotenv ~/devdotenv
+# todo: Configs
 rm -rf tmpbackup
